85c38716e0f58d81bb456d108a90b38b6da95b2a
NirajDalavi <princenirajnsd@gmail.com>
2022-11-08 23:11:48 +0530

    ahhaha

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template,url_for,redirect,flash
+from flask import Flask,render_template,url_for,redirect,flash,request
 from forms import SignupForm,LoginForm
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 app=Flask(__name__)
 app.config['SECRET_KEY']='linktree'
 @app.route('/')
@@ -11,20 +13,42 @@ def home():
 def signup():
     form=SignupForm()
     if form.validate_on_submit():
-        flash(f'Account created successfully for {form.username.data}', category="success")
+        connection=sqlite3.connect("users_data.db")
+        cursor=connection.cursor()
+        sqlite_insert="""INSERT INTO users(name,password) VALUES(?,?);"""
+        username=form.username.data
+        password=form.password.data
+        print(username,password)
+        data_tuple=(username,password)
+        cursor.execute(sqlite_insert,data_tuple)
+        connection.commit()
+        flash(f'Account created successfully for {form.username.data}')
         return redirect(url_for('login'))
     return render_template('signup.html',title='Signup',form=form)
 
+@app.route("/account")
 @app.route("/login",methods=['POST','GET'])
 def login():
-    form=LoginForm()
-    if form.validate_on_submit():
-        if form.email.data=='' and form.data.password.data=='':
-            flash(f'Logged in successfully for {form.username.data}', category="success")
-            return redirect(url_for('account'))
+   
+    if request.method=='POST':
+        connection=sqlite3.connect('users_data.db')
+        cursor=connection.cursor()
+
+        name=request.form['name']
+        password=request.form['password']
+
+        # print(name,password)
+
+        query="SELECT name,password FROM users where name='"+name+"' and password='"+password+"'" 
+        cursor.execute(query)
+
+        results=cursor.fetchall() 
+
+        if len(results)==0:
+            print("Incorrect Credentials provided.Try Again!")
         else:
-            flash(f'Login unsuccessful for {form.username.data}', category="danger")
-            return render_template('login.html',title='login',form=form)
+            return render_template('account.html')
+    return render_template('login.html')           
 
 if __name__=="__main__":
     app.run(debug=True)        
---
+++ create.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+connection=sqlite3.connect("users_data.db")
+cursor=connection.cursor()
+# command="""CREATE TABLE IF NOT EXISTS users(name TEXT, password TEXT)"""
+cursor.execute("INSERT INTO users VALUES ('db','1234567')")
+cursor.execute("INSERT INTO users VALUES ('saathvik','password-saat')")
+cursor.execute("INSERT INTO users VALUES ('aditya','password-raj')")
+# cursor.execute(command)
+connection.commit()
