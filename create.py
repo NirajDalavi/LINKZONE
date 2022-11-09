798d82b8c2a8e726bb0df1a416c9c1b90d011643
NirajDalavi <princenirajnsd@gmail.com>
2022-11-10 00:25:57 +0530

    lol

--- create.py
+++ create.py
@@ -2,9 +2,7 @@ import sqlite3
 
 connection=sqlite3.connect("users_data.db")
 cursor=connection.cursor()
-# command="""CREATE TABLE IF NOT EXISTS users(name TEXT, password TEXT)"""
-cursor.execute("INSERT INTO users VALUES ('db','1234567')")
-cursor.execute("INSERT INTO users VALUES ('saathvik','password-saat')")
-cursor.execute("INSERT INTO users VALUES ('aditya','password-raj')")
-# cursor.execute(command)
+command="""CREATE TABLE IF NOT EXISTS users(name TEXT, password TEXT)"""
+
+cursor.execute(command)
 connection.commit()
