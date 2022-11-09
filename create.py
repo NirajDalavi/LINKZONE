8b41ba25c0ec82dd5008ef35b0b277a085d89fbe
Niraj Dalavi <111214612+NirajDalavi@users.noreply.github.com>
2022-11-10 00:54:28 +0530

    Update create.py

--- create.py
+++ create.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection=sqlite3.connect("users_data.db")
 cursor=connection.cursor()
-command="""CREATE TABLE IF NOT EXISTS users(name TEXT, password TEXT)"""
+command="""CREATE TABLE IF NOT EXISTS users(name TEXT,email TEXT, password TEXT)"""
 
 cursor.execute(command)
 connection.commit()
